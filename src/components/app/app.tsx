ca60f5c80cdc3a010219e725193207bbb603822b
Bulat Khabibullin <bulat.kt@bookmate.com>
2021-09-21 12:23:58 +0300

    dummy pages added

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,16 +1,43 @@
 import React from 'react';
 import appStyles from './app.module.css'
 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import AppHeader from '../app-header/app-header';
 import MainContainer from '../main-container/main-container';
 import { mainTitle } from '../../utils/menu-titles-data';
+import { Login, Register, ForgotPassword, ResetPassword, Profile, NotFoundError } from '../../pages';
 
 function App() {
   return (
-    <div className={appStyles.app} id="app">
-      <AppHeader/>
-      <MainContainer title={mainTitle}/>
-    </div>
+    <Router>
+      <div className={appStyles.app} id="app">
+        <AppHeader/>
+        <Switch>
+          <Route path='/login'>
+            <Login />
+          </Route>
+          <Route path='/register'>
+            <Register />
+          </Route>
+          <Route path='/forgot-password'>
+            <ForgotPassword />
+          </Route>
+          <Route path='/reset-password'>
+            <ResetPassword />
+          </Route>
+          <Route path='/profile'>
+            <Profile />
+          </Route>
+          <Route path='/' exact>
+            <MainContainer title={mainTitle}/>
+          </Route>
+          <Route path='*'>
+            <NotFoundError />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/not-found-error.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function NotFoundError() {
+  return <div>not found page</div>
+}
+
+export default NotFoundError;
