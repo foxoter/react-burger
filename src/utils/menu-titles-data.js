4c1bb45c9fc0c9620dc96dc37206504c02607b9c
Bulat Khabibullin <bulat.kt@bookmate.com>
2021-09-25 13:15:45 +0300

    header navigation implemented

--- src/utils/menu-titles-data.js
+++ src/utils/menu-titles-data.js
@@ -1,8 +1,8 @@
 import { BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 
 export const menuItems = [
-    { title: 'Конструктор', Icon: BurgerIcon },
-    { title: 'Лента заказов', Icon: ListIcon },
-    { title: 'Личный кабинет', Icon: ProfileIcon }
+    { title: 'Конструктор', Icon: BurgerIcon, path: '/' },
+    { title: 'Лента заказов', Icon: ListIcon, path: '/feed' },
+    { title: 'Личный кабинет', Icon: ProfileIcon, path: '/profile' },
 ];
 export const mainTitle = 'Соберите бургер';
