78aeb3a75992de716503fd1f16f76137ff5d7edf
Bulat Khabibullin <bulat.kt@bookmate.com>
2021-10-03 15:39:51 +0300

    token helper edit

--- src/helpers/tokens-helper.tsx
+++ src/helpers/tokens-helper.tsx
@@ -23,7 +23,7 @@ export function setCookie(name: string, value: string, props?: any) {
 
 export function getCookie(name: string) {
   const matches = document.cookie.match(
-    new RegExp('(?:^|; )' + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + '=([^;]*)')
+    new RegExp('(?:^|; )' + name.replace(/([.$?*|{}()[\]\\/+^])/g, '\\$1') + '=([^;]*)')
   );
   return matches ? decodeURIComponent(matches[1]) : undefined;
 }
