76a9f1171b7d94fb6ba04353241b77e7af6eafa3
Bulat Khabibullin <bulat.kt@bookmate.com>
2021-10-04 18:58:23 +0300

    logout actions fix

--- src/services/actions/user.ts
+++ src/services/actions/user.ts
@@ -12,8 +12,8 @@ export const LOGIN_USER_SUCCESS = 'LOGIN_USER_SUCCESS';
 export const LOGIN_USER_FAILED = 'LOGIN_USER_FAILED';
 
 export const LOGOUT_USER_REQUEST = 'LOGOUT_USER_REQUEST';
-export const LOGOUT_USER_SUCCESS = 'LOGOUT_USER';
-export const LOGOUT_USER_FAILED = 'LOGOUT_USER';
+export const LOGOUT_USER_SUCCESS = 'LOGOUT_USER_SUCCESS';
+export const LOGOUT_USER_FAILED = 'LOGOUT_USER_FAILED';
 
 export function registerUser(data: UserDataTypes) {
   return function (dispatch: any) {
