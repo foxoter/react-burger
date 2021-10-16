193de8eaaaa7cdb6a69cad458f5c22e1a93a262f
Bulat Khabibullin <bulat.kt@bookmate.com>
2021-10-16 10:40:57 +0300

    move store init to separate file

--- src/index.tsx
+++ src/index.tsx
@@ -6,18 +6,7 @@ import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import { rootReducer } from './services/reducers';
 import { BrowserRouter as Router } from 'react-router-dom';
-
-declare global {
-  interface Window {
-    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
-  }
-}
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const enhancer = composeEnhancers(applyMiddleware(thunk));
-
-const store = createStore(rootReducer, enhancer);
+import { store } from './services/store';
 
 ReactDOM.render(
   <React.StrictMode>
