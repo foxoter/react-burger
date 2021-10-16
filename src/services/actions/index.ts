fbc0e75654531e5b328dfcb0c937a34adb235523
Bulat Khabibullin <bulat.kt@bookmate.com>
2021-10-16 10:55:43 +0300

    store and thunk types added

--- src/services/actions/index.ts
+++
@@ -1,6 +0,0 @@
-import { TBurgerActions } from './burger';
-import { TIngredientsActions } from './ingredients';
-import { TOrderActions } from './order';
-import { TUserActions } from './user';
-
-export type TAppActions = TBurgerActions | TIngredientsActions | TOrderActions | TUserActions;
