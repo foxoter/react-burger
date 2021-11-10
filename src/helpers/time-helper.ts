d77646b2ae7696912589008d7d8ce5339cb51a51
Bulat Khabibullin <bulat.kt@bookmate.com>
2021-11-10 16:34:13 +0300

    add time parser logic

--- src/helpers/time-helper.ts
+++ src/helpers/time-helper.ts
@@ -1,3 +1,41 @@
+function calculateDaysDiff (date: Date): number {
+  const oneDay = 1000 * 3600 * 24;
+  const today = new Date();
+  return Math.round(Math.abs((today.getTime() - date.getTime()) / oneDay));
+}
+
+function getDateString(diff: number): string {
+  let res = '';
+  switch (diff) {
+    case 0:
+      res = 'Сегодня'
+      break
+    case 1:
+      res = 'Вчера'
+      break
+    case 2:
+      res = 'Позавчера'
+      break
+    case 3:
+    case 4:
+      res = `${diff} дня назад`
+      break
+    default:
+      res = `${diff} дней назад`
+      break
+  }
+  return res;
+}
+
 export function parseTime(timestamp: string): string {
-  return `${timestamp}he`;
+  const dateObj = new Date(Date.parse(timestamp));
+  const hours = dateObj.getHours();
+  const minutes = dateObj.getMinutes() < 10 ? '0' + dateObj.getMinutes() : dateObj.getMinutes();
+  const timezoneDiff = dateObj.getTimezoneOffset();
+  const timezone =
+    timezoneDiff.toString()[0] === '+' ?
+      'i-GMT–' + Math.abs(timezoneDiff / 60)
+      : 'i-GMT+' + Math.abs(timezoneDiff / 60);
+  const day = getDateString(calculateDaysDiff(dateObj));
+  return String(day + ', ' + hours + ':' + minutes + ' ' + timezone);
 }
