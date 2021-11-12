1e6916e9f31e1ea4869a465f9de094df47d9f491
Bulat Khabibullin <bulat.kt@bookmate.com>
2021-11-12 20:33:06 +0300

    burger constructor reducer tests step 1

---
+++ src/services/reducers/burger-reducer.test.js
@@ -0,0 +1,73 @@
+import {
+  ADD_INGREDIENT,
+  DELETE_INGREDIENT,
+  REWRITE_INGREDIENTS,
+} from '../constants/burger';
+import {constructorReducer} from "./burger-reducer";
+
+const mockIngredient = {
+  _id: "60d3b41abdacab0026a733cd",
+  name: "Соус фирменный Space Sauce",
+  type: "sauce",
+  proteins: 50,
+  fat: 22,
+  carbohydrates: 11,
+  calories: 14,
+  price: 80,
+  image: "https://code.s3.yandex.net/react/code/sauce-04.png",
+  image_mobile: "https://code.s3.yandex.net/react/code/sauce-04-mobile.png",
+  image_large: "https://code.s3.yandex.net/react/code/sauce-04-large.png",
+  __v: 0,
+  uuid: "60d3b41abdacab0026a733cd0.09945591567361456",
+}
+
+const mockBunIngredient = {
+  _id: "60d3b41abdacab0026a733c7",
+  name: "Флюоресцентная булка R2-D3",
+  type: "bun",
+  proteins: 44,
+  fat: 26,
+  carbohydrates: 85,
+  calories: 643,
+  price: 988,
+  image: "https://code.s3.yandex.net/react/code/bun-01.png",
+  image_mobile: "https://code.s3.yandex.net/react/code/bun-01-mobile.png",
+  image_large: "https://code.s3.yandex.net/react/code/bun-01-large.png",
+  __v: 0,
+  uuid: "60d3b41abdacab0026a733c70.21754545086208044"
+}
+
+const burgerConstructorInitialState = {
+  constructorItems: [],
+}
+
+describe('burger constructor reducer', () => {
+  it('should return the initial state', () => {
+    expect(constructorReducer(undefined, {})).toEqual(burgerConstructorInitialState);
+  });
+
+  it('should handle ADD_INGREDIENT', () => {
+    expect(constructorReducer(burgerConstructorInitialState,
+      {
+        type: ADD_INGREDIENT,
+        payload: mockBunIngredient,
+      })).toEqual({ constructorItems: [mockBunIngredient, mockBunIngredient] });
+
+    expect(constructorReducer(burgerConstructorInitialState,
+      {
+        type: ADD_INGREDIENT,
+        payload: mockIngredient,
+      })).toEqual({ constructorItems: [mockIngredient] });
+
+    expect(constructorReducer({ constructorItems: [mockIngredient] },
+      {
+        type: ADD_INGREDIENT,
+        payload: mockIngredient,
+      })).toEqual({ constructorItems: [mockIngredient, mockIngredient] });
+  });
+
+  // it('should handle DELETE_INGREDIENT', () => {
+  //
+  // })
+});
+
